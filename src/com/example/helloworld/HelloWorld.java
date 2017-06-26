8924727557b7701da5d52430be5bac301a27540f
Xinyi Peng <peng54@purdue.edu>
2017-06-26 14:02:01 +0800

    first attempt

---
+++ src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.example.helloworld;
+
+/**
+ * Created by PXY on 26/06/2017.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
